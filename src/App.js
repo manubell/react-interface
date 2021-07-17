994836a11c2dbfbbafa5b425ba7f56a957e335f5
manu <manu.bellemans@gmail.com>
2021-07-18 01:23:15 +0200

    add plugin prettier

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import {BiCalendar} from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
-import {useCallback, useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
-
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
 
-  const filteredAppointments = appointmentList.filter(
-    item => {
-      return (
-        item.petName.toLowerCase().includes(query.toLowerCase()) ||
-        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-        item.aptNotes.toLowerCase().includes(query.toLowerCase())
-      );
-    }
-  )
-
+  const filteredAppointments = appointmentList.filter((item) => {
+    return (
+      item.petName.toLowerCase().includes(query.toLowerCase()) ||
+      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+    );
+  });
 
   const fetchData = useCallback(() => {
-    fetch('./data.json')
-      .then(response => response.json())
-      .then(data => setAppointmentList(data))
-  }, [])
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => setAppointmentList(data));
+  }, []);
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, [fetchData]);
 
   return (
     <div className="App container mx-auto mt-3">
       <h1 className="text-5xl mb-3">
-        <BiCalendar className="inline-block text-red-400 align-top"/> Your appointments
+        <BiCalendar className="inline-block text-red-400 align-top" /> Your
+        appointments
       </h1>
 
-      <AddAppointment/>
-      <Search query={query}
-              onQueryChange={myQuery => setQuery(myQuery)}/>
+      <AddAppointment />
+      <Search query={query} onQueryChange={(myQuery) => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gray-200">
-        {filteredAppointments.map(appointment => (
-          <AppointmentInfo key={appointment.id}
-                           appointment={appointment}
-                           onDeleteAppointment={appointmentId => setAppointmentList(
-                             appointmentList.filter(appointment => appointment.id !== appointmentId)
-                           )}
+        {filteredAppointments.map((appointment) => (
+          <AppointmentInfo
+            key={appointment.id}
+            appointment={appointment}
+            onDeleteAppointment={(appointmentId) =>
+              setAppointmentList(
+                appointmentList.filter(
+                  (appointment) => appointment.id !== appointmentId
+                )
+              )
+            }
           />
         ))}
       </ul>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,35 +1,57 @@
-import {BiCaretDown, BiCheck, BiSearch} from "react-icons/bi";
-import {useState} from "react";
+import { BiCaretDown, BiCheck, BiSearch } from "react-icons/bi";
+import { useState } from "react";
 
-const DropDown = ({toggle}) => {
+const DropDown = ({ toggle }) => {
   if (!toggle) {
     return null;
   }
   return (
-    <div className="origin-top-right absolute right-0 mt-2 w-56
-      rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-      <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+    <div
+      className="origin-top-right absolute right-0 mt-2 w-56
+      rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+    >
+      <div
+        className="py-1"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="options-menu"
+      >
         <div
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Pet Name <BiCheck /></div>
+          role="menuitem"
+        >
+          Pet Name <BiCheck />
+        </div>
         <div
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Owner Name  <BiCheck /></div>
+          role="menuitem"
+        >
+          Owner Name <BiCheck />
+        </div>
         <div
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Date <BiCheck /></div>
+          role="menuitem"
+        >
+          Date <BiCheck />
+        </div>
         <div
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
-          role="menuitem">Asc <BiCheck /></div>
+          role="menuitem"
+        >
+          Asc <BiCheck />
+        </div>
         <div
           className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
-          role="menuitem">Desc <BiCheck /></div>
+          role="menuitem"
+        >
+          Desc <BiCheck />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const Search = ({query, onQueryChange}) => {
+const Search = ({ query, onQueryChange }) => {
   const [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
@@ -38,21 +60,37 @@ const Search = ({query, onQueryChange}) => {
           <BiSearch />
           <label htmlFor="query" className="sr-only" />
         </div>
-        <input type="text" name="query" id="query" value={query}
-               onChange={(event) => {onQueryChange(event.target.value)}}
-               className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300" placeholder="Search" />
+        <input
+          type="text"
+          name="query"
+          id="query"
+          value={query}
+          onChange={(event) => {
+            onQueryChange(event.target.value);
+          }}
+          className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300"
+          placeholder="Search"
+        />
         <div className="absolute inset-y-0 right-0 flex items-center">
           <div>
-            <button type="button" onClick={() => {setToggleSort(!toggleSort)}}
-                    className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
+            <button
+              type="button"
+              onClick={() => {
+                setToggleSort(!toggleSort);
+              }}
+              className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
+              id="options-menu"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown toggle={toggleSort}/>
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
