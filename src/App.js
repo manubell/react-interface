b0f6c9dc401d1008f3a8bc98cadbd3de3fc71a95
manu <manu.bellemans@gmail.com>
2021-07-17 23:49:49 +0200

    add react-icons and test an icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import {BiArchive} from "react-icons/bi";
+
 function App() {
   return (
     <div className="App">
-      <h1>Your appointments</h1>
+      <h1><BiArchive /> Your appointments</h1>
     </div>
   );
 }
